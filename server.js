b75376aa9f0eb196cd5ce030ee68a769668174dc
ErickElizondo <erick.elizondo20@gmail.com>
2021-09-10 20:39:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use((req,res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"https://testingproyectofrontend.herokuapp.com");
-    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.setHeader('Access-Control-Allow-Origin',"*");
+    res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });
