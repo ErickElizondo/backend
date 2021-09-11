9eb2347f2619fcd31df5402275144a3e1fc51a17
ErickElizondo <erick.elizondo20@gmail.com>
2021-09-10 20:31:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors({ origin: true }));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
